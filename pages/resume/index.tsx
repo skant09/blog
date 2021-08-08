0ec57598a2ca192c4a8071f0e61285fea61f748e
skant09 <s.kant@outlook.com>
2021-08-08 23:02:45 +0530

    Updating resume WIP

--- pages/resume/index.tsx
+++ pages/resume/index.tsx
@@ -7,7 +7,7 @@ const Resume = (): JSX.Element => {
       <div className={style.resume}>
         <header className={style.header}>
           <div className={style.left}>
-            <strong>Suryakant</strong>
+            <h2>Suryakant</h2>
             <span>Tech Lead </span>
             <span>Frontend@Zivame</span>
           </div>
@@ -19,9 +19,9 @@ const Resume = (): JSX.Element => {
           </div>
         </header>
 
-        <div className={style.body}>
+        <div className={style.resume_body}>
           <section>
-            <strong>Education</strong>
+            <h2>Education</h2>
             <details>
               <summary>
                 <em>Dual Degree, IIT Madras </em>(2004-2009)
@@ -48,21 +48,95 @@ const Resume = (): JSX.Element => {
           </section>
 
           <section>
-            <strong>Experience</strong>
+            <h2>Experience</h2>
             <details>
               <summary>
-                <em>Technology Lead Frontend@Zivame </em>(Jan 2020 - present)
+                <em>
+                  Technology Lead Frontend@<a href="www.zivame.com">Zivame</a>
+                </em>
+                (Jan 2020 - present)
               </summary>
             </details>
-            <details></details>
+
+            <details>
+              <summary>
+                <em>
+                  Senior web developer@<a href="www.amazon.in">Amazon</a>
+                </em>
+                (March 2019 - Dec 2019)
+              </summary>
+            </details>
+
+            <details>
+              <summary>
+                <em>
+                  Technical Product Manager@
+                  <a href="https://www.syook.com/about-us">Syook</a>
+                </em>
+                (May 2018 - March 2019)
+              </summary>
+            </details>
+
+            <details>
+              <summary>
+                <em>
+                  Senior software develoepr@
+                  <a href="https://www.jifflenow.com/">Jifflenow</a>{' '}
+                </em>
+                (April 2017 - April 2018)
+              </summary>
+            </details>
+
+            <details>
+              <summary>
+                <em>
+                  Senior software develoepr@<a href="www.zivame.com">Zivame</a>
+                </em>
+                (Sept 2016 - April 2017)
+              </summary>
+            </details>
+
+            <details>
+              <summary>
+                <em>
+                  Software Consultant@
+                  <a href="https://www.nineleaps.com/">Nineleaps</a>
+                </em>
+                (Apr 2014 - Sept 2016)
+              </summary>
+            </details>
           </section>
 
           <section>
-            <strong>Computer Skill</strong>
+            <h2>Computer Skill</h2>
+
+            <details>
+              <summary>
+                <em>JavaScript</em>
+              </summary>
+            </details>
+
+            <details>
+              <summary>
+                <em>HTML</em>
+              </summary>
+            </details>
+
+            <details>
+              <summary>
+                <em>CSS</em>
+              </summary>
+            </details>
+
+            <details>
+              <summary>
+                <em>Python</em>
+              </summary>
+            </details>
           </section>
 
           <section>
-            <strong>Publications</strong>
+            <h2>Publications</h2>
           </section>
         </div>
       </div>
