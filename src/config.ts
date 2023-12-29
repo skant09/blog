e8bc1fb5f858217e796d7fbdb161caab70657643
Suryakant <surya.k@zivame.com>
2023-12-30 02:56:04 +0530

    astro build

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
   website: "https://skant09.github.io/blog", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  author: "Suryakant",
+  desc: "Personal website",
+  title: "Suryakant",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
